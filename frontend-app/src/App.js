bbfcee988035d50aad9a49f5ff64da68a59fd5b5
Raine Koizumi <rainekoizumi@berkeley.edu>
2022-11-15 03:10:38 -0800

    initialized frontend page with basic button and text

---
+++ frontend-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Image Host API</h1>
+      <Button variant="contained">Load all files</Button>
+    </div>
+  );
+}
+
+export default App;
